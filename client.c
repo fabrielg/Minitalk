6b13793e98a8eb4ec8b49808631dd6d932be263b
fabrielg <gfrancois2003@gmail.com>
2025-02-24 21:00:12 +0100

    feat: client can send message to server and server display the message

--- client.c
+++ client.c
@@ -6,12 +6,40 @@
 /*   By: gfrancoi <gfrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 13:33:45 by gfrancoi          #+#    #+#             */
-/*   Updated: 2025/02/24 15:25:32 by gfrancoi         ###   ########.fr       */
+/*   Updated: 2025/02/24 20:59:19 by gfrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft/libft.h"
 #include <unistd.h>
+#include <signal.h>
+
+void	send_bits(int c, int pid)
+{
+	int	i;
+
+	i = 0;
+	while (i < 8)
+	{
+		if (c & 0b10000000)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		c = c << 1;
+		i++;
+		usleep(1000);
+	}
+}
+
+void	send_str(char *str, int pid)
+{
+	while (*str)
+	{
+		send_bits(*str, pid);
+		str++;
+	}
+	send_bits(0, pid);
+}
 
 int	main(int ac, char **av)
 {
@@ -22,5 +50,6 @@ int	main(int ac, char **av)
 		return (0);
 	server_pid = ft_atoi(av[1]);
 	str = av[2];
+	send_str(str, server_pid);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,18 +6,49 @@
 /*   By: gfrancoi <gfrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 13:33:45 by gfrancoi          #+#    #+#             */
-/*   Updated: 2025/02/24 15:25:24 by gfrancoi         ###   ########.fr       */
+/*   Updated: 2025/02/24 20:59:03 by gfrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft/libft.h"
 #include <unistd.h>
+#include <signal.h>
+#include <bits/sigaction.h>
+
+void	signal_handler(int signum)
+{
+	static int	value = 0b0;
+	static int	power = 0;
+
+	if (signum == SIGUSR1)
+		value += (128 >> power);
+	power++;
+	if (power >= 8)
+	{
+		if (value)
+			ft_putchar_fd(value, 1);
+		else
+			ft_putchar_fd('\n', 1);
+		power = 0;
+		value = 0b0;
+	}
+}
 
 int	main(void)
 {
-	int	pid;
+	int					pid;
+	struct sigaction	sa;
 
 	pid = getpid();
 	ft_printf("\e[%imServer PID : \e[%im%d\n", 32, 33, pid);
+	sa.sa_handler = signal_handler;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	if ((sigaction(SIGUSR1, &sa, 0)) == -1)
+		ft_putendl_fd("Error sigaction", 2);
+	if ((sigaction(SIGUSR2, &sa, 0)) == -1)
+		ft_putendl_fd("Error sigaction", 2);
+	while (1)
+		pause();
 	return (0);
 }

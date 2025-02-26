eeb83b78949805124a07a15112ec6d1af5311b92
fabrielg <gfrancois2003@gmail.com>
2025-02-26 14:27:17 +0100

    feat: add colors in output

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: gfrancoi <gfrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 13:33:45 by gfrancoi          #+#    #+#             */
-/*   Updated: 2025/02/26 11:47:05 by gfrancoi         ###   ########.fr       */
+/*   Updated: 2025/02/26 14:26:05 by gfrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 #include "minitalk.h"
 #include <unistd.h>
 #include <signal.h>
+
+#define INVALID_PID "%sFailed to send byte: Invalid PID %s%d%s\n"
+#define TIMEOUT "%sFailed to send byte: Timeout%s\n"
+#define RECEIVED "%sMessage received !%s\n"
+#define NOT_RECEIVED "%sMessage not received !%s\n"
 
 #define MAX_TRY 100
 
@@ -29,7 +34,7 @@ void	send_bits(int c, int pid)
 		g_bit_waiter = MAX_TRY;
 		if (kill(pid, (!(c & 0b10000000) * (SIGUSR2 - SIGUSR1) + SIGUSR1)))
 		{
-			ft_putstr_fd("Failed to send byte: Invalid PID\n", 1);
+			ft_printf(INVALID_PID, RED_B, YELLOW_B, pid, RESET);
 			exit(EXIT_FAILURE);
 		}
 		c = c << 1;
@@ -38,7 +43,7 @@ void	send_bits(int c, int pid)
 			usleep(100);
 		if (g_bit_waiter == 0)
 		{
-			ft_putstr_fd("Failed to send byte: timeout\n", 1);
+			ft_printf(TIMEOUT, RED_B, RESET);
 			exit(EXIT_FAILURE);
 		}
 		usleep(100);
@@ -61,9 +66,9 @@ void	signal_handler(int signum, siginfo_t *info, void *context)
 	(void)info;
 	if (signum == SIGUSR1)
 		g_bit_waiter = -1;
-	else if (signum == SIGUSR2)
+	else if (signum != SIGUSR2)
 	{
-		ft_putstr_fd("Message received !\n", 1);
+		ft_printf(RECEIVED, GREEN, RESET);
 		exit(EXIT_SUCCESS);
 	}
 }
@@ -80,6 +85,6 @@ int	main(int ac, char **av)
 	init_signal(SIGUSR1, signal_handler);
 	init_signal(SIGUSR2, signal_handler);
 	send_str(str, server_pid);
-	ft_putstr_fd("Message not received !\n", 1);
+	ft_printf(NOT_RECEIVED, RED, RESET);
 	return (0);
 }
